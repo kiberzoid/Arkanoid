8e28b44222c86c331b88792e7b702a360bac9952
denis <yloginy@rambler.ru>
2015-11-25 22:14:52 +0300

    добавлен шарик

---
+++ app/src/main/java/kiberzoid/arkanoid/DrawView.java
@@ -0,0 +1,88 @@
+package kiberzoid.arkanoid;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by 1 on 25.11.2015.
+ */
+public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
+    private Paint p;
+    private DrawThread d_thread;
+    private Ball ball;
+    private int width;
+    private int height;
+
+    public DrawView(Context context) {
+        super(context);
+        this.getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        Rect surfaceFrame = holder.getSurfaceFrame();
+        width = surfaceFrame.width();
+        height = surfaceFrame.height();
+        p = new Paint();
+        p.setColor(Color.BLUE);
+        p.setStrokeWidth(10);
+        ball = new Ball(width / 2, 30, 30, 20, 20);
+        d_thread = new DrawThread(holder);
+        d_thread.setRunning(true);
+        d_thread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while (retry) {
+            try {
+                d_thread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+
+    class DrawThread extends Thread {
+        private boolean running = false;
+        private SurfaceHolder holder;
+
+        public DrawThread(SurfaceHolder holder) {
+            this.holder = holder;
+        }
+
+        public void setRunning(boolean running) {
+            this.running = running;
+        }
+
+        @Override
+        public void run() {
+            Canvas canvas;
+            while (running) {
+                canvas = null;
+                try {
+                    canvas = holder.lockCanvas();
+                    if (canvas == null)
+                        continue;
+                    DrawView.this.ball.update(DrawView.this.width,DrawView.this.height);
+                    DrawView.this.ball.drawBall(canvas, DrawView.this.p);
+                } finally {
+                    if (canvas != null)
+                        holder.unlockCanvasAndPost(canvas);
+                }
+            }
+        }
+    }
+}
